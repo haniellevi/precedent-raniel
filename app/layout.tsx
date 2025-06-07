cb7d5b8f3620d384474bc2ebdcf98f682ab31ba5
Raniel Levi Lemos Lima <rvstecnologia@gmail.com>
2025-06-07 19:33:19 +0000

    Restored to '078b7035d43cd986d389e58b4cbd73b9e47d7c4e'
    Replit-Restored-To: 078b7035d43cd986d389e58b4cbd73b9e47d7c4e

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,13 @@
-
-import "@/app/globals.css";
-import { inter } from "@/app/fonts";
-import { Analytics } from "@vercel/analytics/react";
-import { Metadata } from "next";
-import { cn } from "@/lib/utils";
-import { ClerkProvider } from "@clerk/nextjs";
+import cx from "classnames";
+import { sfPro, inter } from "./fonts";
+import "./globals.css";
+import { ClerkProvider } from '@clerk/nextjs'
 import { Suspense } from "react";
 import NavBar from "@/components/layout/navbar";
 import Footer from "@/components/layout/footer";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Precedent - Building blocks for your Next.js project",
   description:
     "Precedent is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
@@ -18,22 +15,21 @@ export const metadata: Metadata = {
   themeColor: "#FFF",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
-        <body className={cn(inter.variable, "bg-gray-50")}>
+      <html lang="en">
+        <body className={cx(sfPro.variable, inter.variable)}>
           <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
-          <Suspense fallback={<div>A carregar...</div>}>
+          <Suspense fallback={<div>Loading...</div>}>
             <NavBar />
           </Suspense>
-          <main>{children}</main>
+          <main className="relative z-10">{children}</main>
           <Footer />
-          <Analytics />
           <Sonner />
         </body>
       </html>
